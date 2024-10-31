83559bc55f59a4d81cfdad4678787df1228ef47e
Carlos Perales <carlosperales050@gmail.com>
2024-10-30 20:01:12 -0600

    render products

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductsService } from '../../services/product.service';
+import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-products',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss',
 })
-export class ProductsComponent {}
+export class ProductsComponent {
+  products: Product[] = [];
+  constructor(private productService: ProductsService) {}
+
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+  getAllProducts() {
+    return this.productService
+      .getAllSimple()
+      .subscribe((products) => (this.products = products));
+  }
+}
