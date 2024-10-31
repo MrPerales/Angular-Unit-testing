bd97929b92c0beceb09a565a408f489fb598d869
Carlos Perales <carlosperales050@gmail.com>
2024-10-31 16:56:18 -0600

    http Client testing Module

---
+++ src/app/services/product.service.spec.ts
@@ -0,0 +1,61 @@
+import { TestBed } from '@angular/core/testing';
+import { ProductsService } from './product.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { Product } from '../models/product.model';
+import { environment } from '../../environments/environment';
+
+fdescribe('Product Service ', () => {
+  let productService: ProductsService;
+  // para poder hacer moking HttpTestingController
+  let httpController: HttpTestingController;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      // importamos el client testing ya que el servicio lo ocupa
+      imports: [HttpClientTestingModule],
+      providers: [ProductsService],
+    });
+    productService = TestBed.inject(ProductsService);
+    httpController = TestBed.inject(HttpTestingController);
+  });
+
+  it('should be created', () => {
+    expect(productService).toBeTruthy();
+  });
+
+  describe('test for getAllSimple', () => {
+    it('should return a product List ', (doneFn) => {
+      // arrange
+      const mockData: Product[] = [
+        {
+          id: '123',
+          title: 'mock',
+          category: { id: 1, name: 'category 1' },
+          description: 'blablabla....',
+          images: [''],
+          price: 123456,
+        },
+      ];
+      // Act
+      productService.getAllSimple().subscribe((data) => {
+        // Assert
+        // ya recibiriamos la mockData por la htto config
+        expect(data.length).toEqual(data.length);
+        expect(data).toEqual(mockData);
+        doneFn();
+      });
+
+      // http config
+      const url = `${environment.API_URL}/api/v1/products`;
+      // expectOne => url que va a tomar encuenta para que no se hagan peticiones reales a la api
+      // cuando vea que hacemos peticion a esa api lo va interceptar por asi decirlo
+      const req = httpController.expectOne(url);
+      // reemplzamos la info
+      req.flush(mockData);
+      // varificamos si esta montado el mockData correctamente
+      httpController.verify();
+    });
+  });
+});
