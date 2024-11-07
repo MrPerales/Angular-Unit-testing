5403d59078208c9da1b635ef02de0bdca410ae55
Carlos Perales <carlosperales050@gmail.com>
2024-11-07 17:40:00 -0600

    debugElement and ByCss

--- src/app/components/person/person.component.spec.ts
+++ src/app/components/person/person.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PersonComponent } from './person.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 fdescribe('PersonComponent', () => {
   let component: PersonComponent;
@@ -32,5 +34,16 @@ fdescribe('PersonComponent', () => {
     // probamos si la etiqueta p tiene ese texto
     expect(p?.textContent).toEqual('soy un parrafo');
   });
-  //
+  //buena practica testarlo con debug por si tu app no es directamente en un navegador
+  // ya que angular puede ser agnostico
+  it('should have <h3> with "person works!"', () => {
+    // elemento debug del componente
+    const personDebug: DebugElement = fixture.debugElement;
+    // sleccionamos la etiqueta de forma agnostica
+    const h3Debug: DebugElement = personDebug.query(By.css('h3')); //OJO esto no te regresa el elemto html
+    // ya teniendo el elemento nativo se puede testear
+    const h3: HTMLElement = h3Debug.nativeElement;
+    // probamos si la etiqueta p tiene ese texto
+    expect(h3?.textContent).toEqual('person works!');
+  });
 });
