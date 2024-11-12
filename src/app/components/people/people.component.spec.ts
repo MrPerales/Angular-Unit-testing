b8eed4424ca3792e8c21675cf1fae05e45154c5f
Carlos Perales <carlosperales050@gmail.com>
2024-11-11 20:17:02 -0600

    pruebas con componente padre (for)

--- src/app/components/people/people.component.spec.ts
+++ src/app/components/people/people.component.spec.ts
@@ -1,17 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PeopleComponent } from './people.component';
+import { PersonComponent } from '../person/person.component';
+import { Person } from '../../models/person.model';
+import { By } from '@angular/platform-browser';
 
-describe('PeopleComponent', () => {
+fdescribe('PeopleComponent', () => {
   let component: PeopleComponent;
   let fixture: ComponentFixture<PeopleComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PeopleComponent]
-    })
-    .compileComponents();
-    
+      imports: [PeopleComponent, PersonComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(PeopleComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +21,21 @@ describe('PeopleComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should have a list app-person components', () => {
+    // arrange
+    component.people = [
+      new Person('User1', 'lastName1', 30, 95, 1.9),
+      new Person('User2', 'lastName2', 20, 50, 1.3),
+      new Person('User3', 'lastName3', 10, 20, 1.4),
+      new Person('User4', 'lastName4', 15, 15, 1.1),
+    ];
+    fixture.detectChanges(); //detectamos los cambios para que podamos
+    // obtener correctamente el elemento Debug y no obtener el elemento sin los cambios
+    // por eso seleccionamos a los elementos despues
+    // act
+    const debugElement = fixture.debugElement.queryAll(By.css('app-person'));
+    // assert
+    expect(debugElement.length).toEqual(4);
   });
 });
--- src/app/components/people/people.component.ts
+++ src/app/components/people/people.component.ts
@@ -11,5 +11,15 @@ import { Person } from '../../models/person.model';
 })
 export class PeopleComponent {
   constructor() {}
-  person: Person = new Person('Carlos', 'carlos', 30, 95, 1.9);
+  // person: Person = new Person('Carlos', 'carlos', 30, 95, 1.9);
+
+  people: Person[] = [
+    new Person('User1', 'lastName', 30, 95, 1.9),
+    new Person('User2', 'lastName', 20, 50, 1.4),
+  ];
+  selectedPerson: Person | null = null;
+
+  choose(person: Person) {
+    this.selectedPerson = person;
+  }
 }
