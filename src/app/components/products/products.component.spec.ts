c1138412fca7d8695c6e6b159b69bffaddb7ee3e
Carlos Perales <carlosperales050@gmail.com>
2024-11-15 19:14:03 -0600

    test componentes con dependencias

--- src/app/components/products/products.component.spec.ts
+++ src/app/components/products/products.component.spec.ts
@@ -1,23 +1,40 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductsComponent } from './products.component';
+import { CardProductComponent } from '../card-product/card-product.component';
+import { ProductsService } from '../../services/product.service';
+import { generateManyProducts } from '../../models/product.mock';
+import { of } from 'rxjs';
 
-describe('ProductsComponent', () => {
-  let component: ProductsComponent;
+fdescribe('ProductsComponent', () => {
+  let productComponent: ProductsComponent;
   let fixture: ComponentFixture<ProductsComponent>;
+  let productService: jasmine.SpyObj<ProductsService>; //TiPADO de spy
 
   beforeEach(async () => {
+    const productServiceSpy = jasmine.createSpyObj('ProductsService', [
+      'getAll',
+    ]); //spy
     await TestBed.configureTestingModule({
-      imports: [ProductsComponent]
-    })
-    .compileComponents();
-    
+      imports: [ProductsComponent, CardProductComponent],
+      providers: [{ provide: ProductsService, useValue: productServiceSpy }], //
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ProductsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    productComponent = fixture.componentInstance;
+    productService = TestBed.inject(
+      ProductsService
+    ) as jasmine.SpyObj<ProductsService>; //productService como spia
+
+    const producstMock = generateManyProducts(5);
+    // simulamos el ejecutar el servicio ,como nos tiene que retornar un observable usamos "of"
+    // y nos retorna el productMock
+    productService.getAll.and.returnValue(of(producstMock));
+    fixture.detectChanges(); //ngOnInit
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(productComponent).toBeTruthy();
+    expect(productService.getAll).toHaveBeenCalled();
   });
 });
