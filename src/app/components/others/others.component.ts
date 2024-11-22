4319211f817dc52c6462f1d646f63cce27bcf56d
Carlos Perales <carlosperales050@gmail.com>
2024-11-22 17:08:50 -0600

    test pipes

--- src/app/components/others/others.component.ts
+++ src/app/components/others/others.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HighligthDirective } from '../../directives/highligth.directive';
+import { ReversePipe } from '../../pipes/reverse.pipe';
 
 @Component({
   selector: 'app-others',
   standalone: true,
-  imports: [FormsModule, HighligthDirective],
+  imports: [FormsModule, HighligthDirective, ReversePipe],
   templateUrl: './others.component.html',
   styleUrl: './others.component.scss',
 })
 export class OthersComponent {
   color = 'orange';
+  text = 'roma';
 }
