ee7c0c56426bc92cc8b82a50200627d0c2964538
Carlos Perales <carlosperales050@gmail.com>
2024-11-29 18:35:03 -0600

    click event helper

---
+++ src/testing/click.ts
@@ -0,0 +1,34 @@
+import { ComponentFixture } from '@angular/core/testing';
+import { query, queryById } from './finders';
+
+export function clickEvent<T>(
+  fixture: ComponentFixture<T>,
+  selector: string,
+  withTestId: boolean = false,
+  event: unknown = null
+) {
+  let debugElement;
+  if (withTestId) {
+    debugElement = queryById(fixture, selector);
+  } else {
+    debugElement = query(fixture, selector);
+  }
+  debugElement.triggerEventHandler('click', event); //solo ejecuta la accion del boton
+  // mas no lo presiona por asi decirlo
+}
+
+//clickElement simula el presionar el boton
+export function clickElement<T>(
+  fixture: ComponentFixture<T>,
+  selector: string,
+  withTestId: boolean = false
+) {
+  let debugElement;
+  if (withTestId) {
+    debugElement = query(fixture, selector);
+  } else {
+    debugElement = queryById(fixture, selector);
+  }
+  const element: HTMLElement = debugElement.nativeElement;
+  element.click();
+}
