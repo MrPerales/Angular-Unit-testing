f2b55cbcf8497bdde7a37f2489c33a745418a5c5
Carlos Perales <carlosperales050@gmail.com>
2024-12-12 20:22:39 -0600

    product detail render

--- src/app/products/components/card-product/card-product.component.ts
+++ src/app/products/components/card-product/card-product.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { Product } from '../../../models/product.model';
 import { CurrencyPipe, SlicePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-card-product',
   standalone: true,
-  imports: [CurrencyPipe, SlicePipe],
+  imports: [CurrencyPipe, SlicePipe, RouterLink],
   templateUrl: './card-product.component.html',
   styleUrl: './card-product.component.scss',
 })
--- src/app/products/components/product-detail/product-detail.component.ts
+++ src/app/products/components/product-detail/product-detail.component.ts
@@ -1,10 +1,48 @@
+import { CurrencyPipe, Location } from '@angular/common';
 import { Component } from '@angular/core';
+import { ProductsService } from '../../../services/product.service';
+import { Product } from '../../../models/product.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.scss',
 })
-export class ProductDetailComponent {}
+export class ProductDetailComponent {
+  constructor(
+    private route: ActivatedRoute,
+    private productsService: ProductsService,
+    private location: Location
+  ) {}
+  product: Product | null = null;
+  ngOnInit() {
+    this.route.paramMap.subscribe({
+      next: (param) => {
+        const productId = param.get('id');
+        if (productId) {
+          this.getProductDetail(productId);
+        } else {
+          this.goToBack();
+        }
+      },
+    });
+  }
+
+  private getProductDetail(productId: string) {
+    this.productsService.getOne(productId).subscribe({
+      next: (product) => {
+        this.product = product;
+      },
+      error: (error) => {
+        console.log(error);
+        this.goToBack();
+      },
+    });
+  }
+  goToBack() {
+    this.location.back();
+  }
+}
