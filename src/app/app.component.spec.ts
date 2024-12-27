e9c56bcc9fb37d5642800124f08b6517ef7df157
Carlos Perales <carlosperales050@gmail.com>
2024-12-27 17:40:12 -0600

    tests routerlink

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,44 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { provideRouter, RouterLink } from '@angular/router';
+import { By } from '@angular/platform-browser';
 
-xdescribe('AppComponent', () => {
+fdescribe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, RouterLink],
+      providers: [provideRouter([])],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have the 'testing-services' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('testing-services');
-  });
+  it('should there are 6 routerLinks', () => {
+    // fixture.detectChanges();
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, testing-services'
-    );
+    const linkDes = fixture.debugElement.queryAll(By.directive(RouterLink));
+    const routes = linkDes.map((link) => link.injector.get(RouterLink));
+    expect(routes.length).toEqual(6);
+  });
+  it('should there are 6 routerLinks and macht with routes', () => {
+    // fixture.detectChanges();
+
+    const linkDes = fixture.debugElement.queryAll(By.directive(RouterLink));
+    const routes = linkDes.map((link) => link.injector.get(RouterLink));
+    expect(routes[0].href).toEqual('/');
+    expect(routes[1].href).toEqual('/products');
+    expect(routes[2].href).toEqual('/pico-preview');
+    expect(routes[3].href).toEqual('/people');
+    expect(routes[4].href).toEqual('/others');
+    expect(routes[5].href).toEqual('/auth/register');
   });
 });
