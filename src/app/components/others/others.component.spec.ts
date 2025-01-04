710ac7c86e53c77a73c84d6e320ed5767a483a1c
Carlos Perales <carlosperales050@gmail.com>
2025-01-03 20:31:40 -0600

    fix test en othersComponent

--- src/app/components/others/others.component.spec.ts
+++ src/app/components/others/others.component.spec.ts
@@ -1,23 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OthersComponent } from './others.component';
+import { ProductsService } from '../../services/product.service';
+import { generateManyProducts } from '../../models/product.mock';
+import { mockObservable } from '../../../testing';
 
 describe('OthersComponent', () => {
   let component: OthersComponent;
   let fixture: ComponentFixture<OthersComponent>;
+  let productService: jasmine.SpyObj<ProductsService>;
 
   beforeEach(async () => {
+    const productServiceSpy = jasmine.createSpyObj('ProductsService', [
+      'getAll',
+    ]);
     await TestBed.configureTestingModule({
-      imports: [OthersComponent]
-    })
-    .compileComponents();
-    
+      imports: [OthersComponent],
+      providers: [{ provide: ProductsService, useValue: productServiceSpy }],
+    }).compileComponents();
     fixture = TestBed.createComponent(OthersComponent);
     component = fixture.componentInstance;
+    productService = TestBed.inject(
+      ProductsService
+    ) as jasmine.SpyObj<ProductsService>;
+    // mokeamos al pricnipio ya que se ejecuta en el ngOnInit
+    const productMock = generateManyProducts(3);
+    productService.getAll.and.returnValue(mockObservable(productMock));
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(productService.getAll).withContext('call').toHaveBeenCalled();
+    expect(component).withContext('create').toBeTruthy();
   });
 });
